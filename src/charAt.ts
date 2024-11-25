574578d8f27f53c4b274564ab3f49c5a9168399b
b.inertia <tap.it.catcat@gmail.com>
2024-11-25 17:56:40 +0700

    fixed error in case NaN as argument in _charAt

--- src/charAt.ts
+++ src/charAt.ts
@@ -1,6 +1,7 @@
+import { _isNaN } from "./isNaN";
 //prototype charAt(string, pointer)
 export function _charAt(value: string, index: number) : string {
-	if (index < 0 || index >= value.length)
+	if (_isNaN(index) || index < 0 || index >= value.length)
 		return ("");
 	return value[index];
 }
